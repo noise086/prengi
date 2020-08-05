9fc0e973e85aa1b45efd5eff5cb9640ac011d475
Noise <noise086@mail.ru>
2020-08-05 15:40:02 +0300

    add slick-slider

--- src/js/script.js
+++ src/js/script.js
@@ -4,44 +4,44 @@ $(document).ready(function() {
 	
 		
 
-	function validateForm(form) {
-		$(form).validate({
+	// function validateForm(form) {
+	// 	$(form).validate({
 
-			debug: true,
-			rules: {
-				name: {
-					required: true,
-					minlength: 2
-				},
-				phone: "required",
-				email: {
-				required: true,
-				email: true
-				}
-			},
-			messages: {
-				name: "Введите Ваше Имя",
-				phone: "Введите Ваш номер телефона",
-				email: {
-					required: "Введите Ваш Email",
-					email: "Формат Email должен быть name@domain.com"
-				}
-				}
-			});
-		}
+	// 		debug: true,
+	// 		rules: {
+	// 			name: {
+	// 				required: true,
+	// 				minlength: 2
+	// 			},
+	// 			phone: "required",
+	// 			email: {
+	// 			required: true,
+	// 			email: true
+	// 			}
+	// 		},
+	// 		messages: {
+	// 			name: "Введите Ваше Имя",
+	// 			phone: "Введите Ваш номер телефона",
+	// 			email: {
+	// 				required: "Введите Ваш Email",
+	// 				email: "Формат Email должен быть name@domain.com"
+	// 			}
+	// 			}
+	// 		});
+	// 	}
 
-	validateForm('#price-form form');
-	validateForm('#quastions-form form');
+	// validateForm('#price-form form');
+	// validateForm('#quastions-form form');
 
-	$('.feed__carousel').slick({
+	$('.promo__slider').slick({
 		// centerMode: true,
 		// centerPadding: '60px',
 		dots: false,
 		speed: 1200,
 		// fade: true,
 		dots: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="icons/feedback/arrow_left.png"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="icons/feedback/arrow_right.png"></button>',
+		// prevArrow: '<button type="button" class="slick-prev"><img src="icons/feedback/arrow_left.png"></button>',
+		// nextArrow: '<button type="button" class="slick-next"><img src="icons/feedback/arrow_right.png"></button>',
 		slidesToShow: 1,
 	  });
 		
