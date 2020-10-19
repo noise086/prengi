be468303c7d54b1fe7cab8bacd94d81718ab93c9
Nikolay <noise086@mail.ru>
2020-10-19 16:28:50 +0400

    add modul slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,45 +1,7 @@
+import slider from './moduls/slider';
+
 $(document).ready(function() {
 	
-	// $('#price__form form').validate();
-	
-		
-
-	// function validateForm(form) {
-	// 	$(form).validate({
-
-	// 		debug: true,
-	// 		rules: {
-	// 			name: {
-	// 				required: true,
-	// 				minlength: 2
-	// 			},
-	// 			phone: "required",
-	// 			email: {
-	// 			required: true,
-	// 			email: true
-	// 			}
-	// 		},
-	// 		messages: {
-	// 			name: "Введите Ваше Имя",
-	// 			phone: "Введите Ваш номер телефона",
-	// 			email: {
-	// 				required: "Введите Ваш Email",
-	// 				email: "Формат Email должен быть name@domain.com"
-	// 			}
-	// 			}
-	// 		});
-	// 	}
-
-	// validateForm('#price-form form');
-	// validateForm('#quastions-form form');
-
-	$('.promo__slider').slick({
-		dots: false,
-		speed: 1200,
-		dots: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/arrowLeft.png"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/arrowRight.png"></button>',
-		slidesToShow: 1,
-	  });
+	slider('.promo__slider_view', '.promo__slider_field', '.promo__slider_item');
 		
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,100 @@
+import slider from './moduls/slider';
 $(document).ready(function() {
+	// Slider
+	// function slider (wrapperSelector, fieldSelector, slidesSelector) {
+	// 	const wrapper = document.querySelector(wrapperSelector),
+	// 		  field = document.querySelector(fieldSelector),
+	// 		  slides = document.querySelectorAll(slidesSelector),
+	// 		  width = window.getComputedStyle(wrapper).width;
 	
-	// $('#price__form form').validate();
+	// 		console.log(width);
+	// 	let offset = 0;
+	// 	let counter = 1;
 	
-		
-
-	// function validateForm(form) {
-	// 	$(form).validate({
-
-	// 		debug: true,
-	// 		rules: {
-	// 			name: {
-	// 				required: true,
-	// 				minlength: 2
-	// 			},
-	// 			phone: "required",
-	// 			email: {
-	// 			required: true,
-	// 			email: true
-	// 			}
-	// 		},
-	// 		messages: {
-	// 			name: "Введите Ваше Имя",
-	// 			phone: "Введите Ваш номер телефона",
-	// 			email: {
-	// 				required: "Введите Ваш Email",
-	// 				email: "Формат Email должен быть name@domain.com"
-	// 			}
-	// 			}
+	// 	// wrapper.style.width = '100%';
+	// 	field.style.width = 100 * slides.length + '%';
+	
+	// 	slides.forEach(item => {
+	// 		item.style.width = width;
+	// 	});
+	
+	// 	field.style.display = 'flex';
+	// 	field.style.transition = '1s all';
+	// 	wrapper.style.overflow = 'hidden';
+	// 	wrapper.style.position = 'relative';
+	
+	// 	const dots = document.createElement('ol');
+	// 	const prev = document.createElement('div');
+	// 	const next = document.createElement('div');
+	// 	prev.classList.add('promo__slider_prev');
+	// 	next.classList.add('promo__slider_next');
+	// 	prev.innerHTML = '<img src="./icons/slider/arrowLeft.png" alt="left">';
+	// 	next.innerHTML = '<img src="./icons/slider/arrowRight.png" alt="right">';
+	// 	wrapper.append(dots);
+	// 	const dotsArr = [];
+	// 	dots.classList.add('promo__slider_dots');
+	// 	slides.forEach((item, i) => {
+	// 		const dot = document.createElement('li');
+	// 		dots.append(dot);
+	// 		dot.setAttribute('data-slide', i+1);
+	// 		dotsArr[i] = dot;
+	// 	});
+	
+	// 	dots.insertAdjacentElement("afterbegin", prev);
+	// 	dots.insertAdjacentElement("beforeend", next);
+	
+	// 	function delLetter(arg) {
+	// 		return +(arg.replace(/\D/g, ''));
+	// 	}
+	
+	// 	function setActiveDot () {
+	// 		dotsArr.forEach(item => {
+	// 			item.classList.remove('promo__slider_active');
+	// 			if (item.getAttribute('data-slide') == counter) {
+	// 				item.classList.add('promo__slider_active');
+	// 			console.log(item);
+	// 		}
 	// 		});
 	// 	}
-
-	// validateForm('#price-form form');
-	// validateForm('#quastions-form form');
-
-	$('.promo__slider').slick({
-		dots: false,
-		speed: 1200,
-		dots: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/arrowLeft.png"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/arrowRight.png"></button>',
-		slidesToShow: 1,
-	  });
 		
+	// 	prev.addEventListener('click', e => {
+	// 		if (offset == 0) {
+	// 			offset = delLetter(width) * (slides.length - 1);
+	// 			counter = slides.length;
+	// 		} else {
+	// 			offset -= delLetter(width);
+	// 			counter--;
+	// 		}
+	// 		field.style.transform = `translateX(-${offset}px)`;
+	// 		console.log(offset);
+	// 		setActiveDot();
+	// 	});
+	
+	// 	next.addEventListener('click', e => {
+	// 		if (offset == delLetter(width) * (slides.length - 1)) {
+	// 			offset = 0;
+	// 			counter = 1;
+	// 		} else {
+	// 			offset += delLetter(width);
+	// 			counter++;
+	// 		}
+	// 		field.style.transform = `translateX(-${offset}px)`;
+	// 		console.log(offset);
+	// 		setActiveDot();
+	// 	});
+	
+	// 	dots.addEventListener('click', e => {
+	// 			const slideTo = e.target.getAttribute('data-slide');
+	// 			if (e.target.getAttribute('data-slide')){
+	// 			offset = delLetter(width) * (slideTo -1);
+	// 			counter = slideTo;
+	// 			setActiveDot();
+	// 			console.log(offset);
+	// 			field.style.transform = `translateX(-${offset}px)`;
+	// 		}
+	// 	});
+	// 	setActiveDot();
+	// }
+	slider('.promo__slider_view', '.promo__slider_field', '.promo__slider_item');
+	
 });
